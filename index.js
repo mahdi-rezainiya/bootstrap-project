8c484dfb228c38c585dc0385a51550b071ab8284
mahdi rezainiya <mahdi.rezainiya@gmail.com>
2023-07-24 14:17:33 +0330

    New Changes

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ const tooltips = document.querySelectorAll('.tt');
 tooltips.forEach(t =>{
     new bootstrap.Tooltip(t)
 })
+
+// bootstrap own JS code 
+
+// document.addEventListener('mouseenter', function() {
+//     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+//     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+// });
