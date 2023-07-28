5cc1fd7a38d63df6cc470079b697b0382d168684
mahdi rezainiya <mahdi.rezainiya@gmail.com>
2023-07-28 16:34:40 +0330

    new project of bootstrap

---
+++ bootstrap2/index.js
@@ -0,0 +1,11 @@
+const SearchIcon = document.getElementById('basic-addon1');
+const SearchInput = document.getElementById('input-addon1');
+
+SearchIcon.addEventListener('click' , e  =>{
+    SearchIcon.style.display = 'none';
+    SearchInput.style.display = 'block'
+    SearchIcon.style.border = '1px solid white';
+    SearchInput.style.border = '1px solid white'
+    e.preventDefault()
+    
+})
